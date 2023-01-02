839f412de4d9a74c6835d33732787baadeef4365
gpopesc <gpopesc@gmail.com>
2023-01-03 00:05:06 +0200

    Add files via upload
    install script

---
+++ install_module.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+cp ./wireguard.ko /lib/modules/
+chmod 644 /lib/modules/wireguard.ko
+
+# register the module
+insmod /lib/modules/wireguard.ko
+
+#autorun at next restart
+sudo tee /usr/local/etc/rc.d/wireguard.sh <<EOF
+    #!/bin/bash
+    MODULES_UNLOAD="wireguard.ko"
+    start_modules(){
+        echo "--- Load module ---"
+        echo "Loading wireguard.ko"
+        insmod /lib/modules/wireguard.ko
+    }
+    stop_modules(){
+        echo "--- Unload modules ---"
+        for i in $MODULES_UNLOAD; do
+            echo "Unloading $i"
+            rmmod $i
+        done
+    }
+    case "$1" in
+    start)
+        start_modules
+        ;;
+    stop)
+        stop_modules
+        ;;
+    *)
+        echo "usage: $0 { start | stop }" >&2
+        exit 1
+        ;;
+    esac
+EOF
+
+chmod 644 /usr/local/etc/rc.d/wireguard.sh
